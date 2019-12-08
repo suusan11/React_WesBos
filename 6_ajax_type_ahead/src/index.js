61c99c7f92f5a768ac2b0ad41f41b8cb9300800e
suusan11 <gawow11@gmail.com>
2019-12-08 10:50:59 -0800

    create new folder

---
+++ 6_ajax_type_ahead/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hi</div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
